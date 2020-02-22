f674ea2f95db6f80be01660f2134ec8ec3de0984
James <conwayjw97@gmail.com>
2020-02-22 17:01:34 +0000

    Added convenience DB viewer script

---
+++ ViewDB.py
@@ -0,0 +1,9 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["WebScienceAssessment"]
+collection = db["tweets"]
+tweets = list(collection.find({}))
+
+for tweet in tweets:
+    print(tweet)
