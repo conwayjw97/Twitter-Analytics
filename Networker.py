4fdb115bf2c05a41d14fd06f60538f0aaf79e175
James <conwayjw97@gmail.com>
2020-03-15 14:59:11 +0000

    Hashtag occurence graphs added

--- Networker.py
+++ Networker.py
@@ -31,8 +31,9 @@ def save_graph(graph, file_name):
 
 def general_reply_graph(collection):
     print("Constructing general reply graph...", end="")
-    reply_graph = nx.DiGraph()
     users = list(collection.find({}).distinct("user"))
+    tweets = list(collection.find({}))
+    reply_graph = nx.DiGraph()
     reply_graph.add_nodes_from(users)
     for tweet in tweets:
         if(("replying_to_user" in tweet) and len(tweet["replying_to_user"]) > 0):
@@ -59,8 +60,9 @@ def cluster_reply_graphs(collection):
 
 def general_mention_graph(collection):
     print("Constructing general mention graph...", end="")
-    mention_graph = nx.DiGraph()
     users = list(collection.find({}).distinct("user"))
+    tweets = list(collection.find({}))
+    mention_graph = nx.DiGraph()
     mention_graph.add_nodes_from(users)
     for tweet in tweets:
         if(("mentioned_users" in tweet) and len(tweet["mentioned_users"]) > 0):
@@ -89,8 +91,9 @@ def cluster_mention_graphs(collection):
 
 def general_retweet_graph(collection):
     print("Constructing general retweet graph...", end="")
-    retweet_graph = nx.DiGraph()
     users = list(collection.find({}).distinct("user"))
+    tweets = list(collection.find({}))
+    retweet_graph = nx.DiGraph()
     retweet_graph.add_nodes_from(users)
     for tweet in tweets:
         if(("retweeted_user" in tweet) and len(tweet["retweeted_user"]) > 0):
@@ -115,6 +118,41 @@ def cluster_retweet_graphs(collection):
         print("Done!")
     return(cluster_reply_graphs)
 
+def general_hashtag_graph(collection):
+    print("Constructing general hashtag graph...", end="")
+    hashtags = list(collection.find({}).distinct("hashtags"))
+    tweets = list(collection.find({}))
+    hashtag_graph = nx.Graph()
+    hashtag_graph.add_nodes_from(hashtags)
+    for tweet in tweets:
+        if(("hashtags" in tweet) and len(tweet["hashtags"]) > 0):
+            for hashtag1 in tweet["hashtags"]:
+                for hashtag2 in tweet["hashtags"]:
+                    if(hashtag1 != hashtag2):
+                        hashtag_graph.add_edge(hashtag1, hashtag2)
+    print("Done!")
+    return hashtag_graph
+
+def cluster_hashtag_graphs(collection):
+    print("Constructing cluster hashtag graphs...")
+    clusters = list(collection.distinct("cluster"))
+    cluster_hashtag_graphs = []
+    for cluster in sorted(clusters):
+        print("Constructing hashtag graph for cluster %d..." % cluster, end="")
+        cluster_tweets = list(collection.find({"cluster":cluster}))
+        cluster_hashtags = list(collection.find({"cluster":cluster}).distinct("hashtags"))
+        hashtag_graph = nx.Graph()
+        hashtag_graph.add_nodes_from(cluster_hashtags)
+        for tweet in cluster_tweets:
+            if(("hashtags" in tweet) and len(tweet["hashtags"]) > 0):
+                for hashtag1 in tweet["hashtags"]:
+                    for hashtag2 in tweet["hashtags"]:
+                        if(hashtag1 != hashtag2):
+                            hashtag_graph.add_edge(hashtag1, hashtag2)
+        cluster_hashtag_graphs.append(hashtag_graph)
+        print("Done!")
+    return(cluster_hashtag_graphs)
+
 if(len(sys.argv) - 1 < 1):
     print("Please run this program with arguments: Networker.py <Network_Type>")
     print("\n<Network_Type> choices:")
@@ -130,7 +168,6 @@ else:
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["WebScienceAssessment"]
     collection = db["tweets"]
-    tweets = list(collection.find({}))
     GRAPH_CHOICE = int(sys.argv[1])
 
     # Construct general reply interaction graph
@@ -159,8 +196,8 @@ else:
 
     # Construct general retweet interaction graph
     elif(GRAPH_CHOICE == 7):
-        retweet_graph = general_hashtag_graph(collection)
+        hashtag_graph = general_hashtag_graph(collection)
 
     # Construct cluster retweet interaction graphs
-elif(GRAPH_CHOICE == 8):
-        cluster_retweet_graphs = cluster_hashtag_graphs(collection)
+    elif(GRAPH_CHOICE == 8):
+        cluster_hashtag_graphs = cluster_hashtag_graphs(collection)
